95da4e29562257a4a225a759de802f67d69a0ed8
Ahmed Loay <ahmed@shahwan.me>
2019-02-18 21:30:43 +0200

    Customer Orders
    now customers can place orders using the checkout button in the cart_detail page
    and fill-in their information, and the order will be placed and stored in the DB

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import OrderItem
+from .forms import OrderCreateForm
+
+from cart.cart import Cart
+
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                # add each item in the order to the db
+                OrderItem.objects.create(
+                    order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+            # clear the cart
+            cart.clear()
+            return render(request, 'orders/order/created.html', {'order': order})
+    else:
+        form = OrderCreateForm()
+    return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
