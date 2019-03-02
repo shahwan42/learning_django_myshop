ae131e55f97ae5b3eac6bff24517434b4d1c6119
Ahmed Shahwan <ahmed@shahwan.me>
2019-03-02 20:00:48 +0200

    coupoun model, to admin panel

--- coupons/admin.py
+++ coupons/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Coupon
+
+
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ['code', 'valid_from', 'valid_to', 'discount', 'active']
+    list_filter = ['active', 'valid_from', 'valid_to']
+    search_fields = ['code']
+admin.site.register(Coupon, CouponAdmin)
