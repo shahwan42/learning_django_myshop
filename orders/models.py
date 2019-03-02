cfe4ae00ddd50640dd60addf5237af83ab026831
Ahmed Shahwan <ahmed@shahwan.me>
2019-03-02 22:33:23 +0200

    coupons works with orders

--- orders/models.py
+++ orders/models.py
@@ -1,8 +1,11 @@
 """Orders Models."""
 
+from decimal import Decimal
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 from shop.models import Product
+from coupons.models import Coupon
 
 
 class Order(models.Model):
@@ -18,6 +21,8 @@ class Order(models.Model):
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
     braintree_id = models.CharField(max_length=150, blank=True)
+    coupon = models.ForeignKey(Coupon, related_name='orders', null=True, blank=True, on_delete=models.SET_NULL)
+    discount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
 
     class Meta:
         ordering = ('-created',)
@@ -26,7 +31,8 @@ class Order(models.Model):
         return f'Order {self.id}'
 
     def get_total_cost(self):
-        return sum(item.get_cost() for item in self.items.all())
+        total_cost = sum(item.get_cost() for item in self.items.all())
+        return total_cost - total_cost * (self.discount / Decimal('100'))  # total cost after discount
 
 
 class OrderItem(models.Model):
