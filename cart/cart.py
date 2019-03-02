0292bfb4fda966418d347ce56e086ded661e1500
Ahmed Shahwan <ahmed@shahwan.me>
2019-03-02 22:04:49 +0200

    coupons works with the cart

--- cart/cart.py
+++ cart/cart.py
@@ -3,6 +3,7 @@
 from decimal import Decimal
 from django.conf import settings
 from shop.models import Product
+from coupons.models import Coupon
 
 
 class Cart():
@@ -16,6 +17,8 @@ class Cart():
             #  save an empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+        # store current applied coupon
+        self.coupon_id = self.session.get('coupon_id')
 
     def __iter__(self):
         """Iterate over the items in the cart and get the products from the database."""
@@ -67,3 +70,17 @@ class Cart():
         """Remove cart from session."""
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            return Coupon.objects.get(id=self.coupon_id)
+        return None
+
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal('100')) * self.get_total_price()
+        return Decimal('0')
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
